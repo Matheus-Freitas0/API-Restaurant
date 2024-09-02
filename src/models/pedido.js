adeb36df91f477b8765630438e366945c2257dc8
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-02 13:33:54 -0300

    pedidos has been implemented

---
+++ src/models/pedido.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Pedido = sequelize.define('Pedido', {
+    cliente: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    itens: {
+        type: DataTypes.JSON,
+        allowNull: false
+    },
+    total: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.STRING,
+        defaultValue: 'pendente'
+    }
+}, {
+    tableName: 'pedidos'
+})
+
+module.exports = Pedido
